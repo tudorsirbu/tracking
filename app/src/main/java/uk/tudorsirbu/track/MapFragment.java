7c37e78be71de246220bce4252fa8774d610b2d5
Tudor Sirbu <tudor.sirbu@gmail.com>
2017-06-21 01:22:14 +0100

    work on adding google maps

--- app/src/main/java/uk/tudorsirbu/track/MapFragment.java
+++ app/src/main/java/uk/tudorsirbu/track/MapFragment.java
@@ -2,6 +2,7 @@ package uk.tudorsirbu.track;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,17 +14,23 @@ import android.view.ViewGroup;
  */
 public class MapFragment extends Fragment {
 
-
-    public MapFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mainMap);
+        mapFragment.getMapAsync(MapInFragment.this);
+
+        return  view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        MapFragment mapFragment = (MapFragment) getActivity()
+                                                .getFragmentManager()
+                                                .findFragmentById(R.id.fragment_view_map);
+
+    }
 }
