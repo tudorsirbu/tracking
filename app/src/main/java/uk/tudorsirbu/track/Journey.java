7784549f964a382cd5cce8f726d80df1572524fa
Tudor Sirbu <tudor.sirbu@gmail.com>
2017-06-23 20:49:25 +0100

    Added model for the journey

--- app/src/main/java/uk/tudorsirbu/track/Journey.java
+++ app/src/main/java/uk/tudorsirbu/track/Journey.java
@@ -1,8 +1,35 @@
 package uk.tudorsirbu.track;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 /**
  * Created by tudorsirbu on 23/06/2017.
  */
 
 public class Journey {
+
+    private String mId;
+    private long mStart;
+    private long mEnd;
+
+    private ArrayList<LatLng> mLocations;
+
+    public Journey() {
+        mId = UUID.randomUUID().toString();
+        mStart = System.currentTimeMillis();
+        mLocations = new ArrayList<LatLng>();
+    }
+
+    public void addLocation(LatLng latLng){
+        mLocations.add(latLng);
+    }
+
+    public void end(){
+        mEnd = System.currentTimeMillis();
+    }
+
+
 }
